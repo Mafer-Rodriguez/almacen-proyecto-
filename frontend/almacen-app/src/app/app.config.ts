bf6a471903bf02db371066078b757509c034cdcf
Mafer-Rodriguez <utm22030609@utma.edu.mx>
2026-03-18 14:57:21 -0600

    feat: estructura inicial frontend Angular con componentes, servicios, guards y modelos

--- frontend/almacen-app/src/app/app.config.ts
+++ frontend/almacen-app/src/app/app.config.ts
@@ -1,11 +1,18 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { routes } from './app.routes';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),//permite hacer peticiones HTTP a tu API de Node.js.
+    provideFirebaseApp(() => initializeApp(environment.firebase)),//inicializa la aplicación de Firebase con la configuración proporcionada en el archivo environment.ts.
+    provideAuth(() => getAuth()),//configura el servicio de autenticación de Firebase, permitiendo que tu aplicación maneje la autenticación de usuarios.
+    provideFirestore(() => getFirestore()),//configura el servicio de Firestore, que es la base de datos en tiempo real de Firebase, permitiendo que tu aplicación interactúe con la base de datos para almacenar y recuperar datos.
   ]
 };
---
+++ frontend/almacen-app/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:3000/api',
+    firebase: {
+        apiKey: "AIzaSyBj-b6Mje9ZnnpLaKaTZIBSOcS5kltQpOo",
+        authDomain: "almacen-app-a8e30.firebaseapp.com",
+        projectId: "almacen-app-a8e30",
+        storageBucket: "almacen-app-a8e30.firebasestorage.app",
+        messagingSenderId: "310353864576",
+        appId: "1:310353864576:web:713172d6a6fb4ba9440365"
+
+    }
+};
