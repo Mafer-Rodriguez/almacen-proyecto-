28fba2fac9cd4ff570d3b69006325d8548bc0695
Mafer-Rodriguez <utm22030609@utma.edu.mx>
2026-03-19 12:38:03 -0600

    feat: pantalla de dashboard con navegacion y pie de pagina

--- frontend/almacen-app/src/app/components/dashboard/dashboard.component.ts
+++ frontend/almacen-app/src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { Usuario } from '../../models/usuario';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
+  usuario: Usuario | null = null;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.authService.usuarioActual$.subscribe(user => {
+      this.usuario = user;
+    });
+  }
+
+  irA(ruta: string) {
+    this.router.navigate([ruta]);
+  }
+
+  cerrarSesion() {
+    this.authService.cerrarSesion();
+  }
 }
